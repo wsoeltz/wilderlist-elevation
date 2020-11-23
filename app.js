9b102e3cd4fafb96197a85c4b63222a970ea9c96
Kyle Soeltz <wsoeltz@gmail.com>
2020-11-23 18:41:58 -0500

    Add simple get request

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
+require('dotenv').config();
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,12 +14,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get('/', (req, res) => {
+  try {
+    const lat = req.query && req.query.lat ? req.query.lat : undefined;
+    const lng = req.query && req.query.lng ? req.query.lng : undefined;
+    res.json({lat, lng});
+  } catch (err) {
+    res.status(500);
+    res.send(err);
+  }
 });
 
 // error handler
@@ -32,10 +30,15 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
 });
+
+if (process.env.NODE_ENV === 'development') {
+  app.listen(5000, () => {
+    // tslint:disable-next-line
+    return console.log(`listening on port 5000`);
+  });
+}
+
+
 
 module.exports = app;
